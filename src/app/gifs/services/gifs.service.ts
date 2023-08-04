b49d210dce7309daa8f39249318adfe78ffe4c1e
ANDREE MATEO NARVAEZ ORTIZ <andreenarvaezmo@unimagdalena.edu.co>
2023-08-04 17:25:04 -0500

    leer localStorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,18 +1,61 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({providedIn: 'root'})
 export class GifsService {
 
+    public gifList: Gif[]=[];
     private _tagsHistory: string[]=[];
 
-    constructor() { }
+    private ApiKey:string='zIhYzN5exZwUdqmJcRMjHC2Op1fFwG4x';
+    private serviceUrl:string='https://api.giphy.com/v1/gifs'
+    
+    constructor(private http: HttpClient) { 
+        this.loadLocalStorage();
+        if(this.tagsHistory.length>0)this.searchTag(this.tagsHistory[0]);
+        console.log("ready")
+    }
     
     get tagsHistory(){
         return [...this._tagsHistory];
     }
 
-    public searchTag(tag:string):void{
-        this._tagsHistory.unshift(tag);
-        console.log(this._tagsHistory)
+    private organizeHistory(tag:string){
+        tag = tag.toLowerCase();
+        if(this._tagsHistory.includes(tag)){
+            this._tagsHistory = this._tagsHistory.filter(x=> x!== tag);
+        }
+        this._tagsHistory.unshift( tag );
+
+        this._tagsHistory =this.tagsHistory.splice(0,10);
+        this.saveLocalStorage();
     }
+
+    private saveLocalStorage():void{
+        localStorage.setItem('history',JSON.stringify( this._tagsHistory))
+    }
+
+    private loadLocalStorage():void{
+        if(!localStorage.getItem('history')) return;
+        this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+    }
+
+    searchTag(tag:string):void{
+        
+        if(tag.length==0)return;
+
+        const params = new HttpParams()
+        .set('api_key',this.ApiKey)
+        .set('q',tag)
+        .set('limit', '10');
+        this.organizeHistory(tag);
+        this.http.get<SearchResponse>(`${this.serviceUrl}/search?`,{params})
+            .subscribe((resp:SearchResponse)=>{
+                this.gifList=resp.data;
+            })
+        
+    }
+
+
 }
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GifsService } from 'src/app/gifs/services/gifs.service';
 
 @Component({
   selector: 'shared-sidebar',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class SidebarComponent {
 
+  constructor(private gifsService: GifsService){
+
+  }
+
+  get tags():string[]{
+    return [...this.gifsService.tagsHistory]
+  }
+
+  selectTag(tag:string):void{ 
+    this.gifsService.searchTag(tag);
+  }
 }
